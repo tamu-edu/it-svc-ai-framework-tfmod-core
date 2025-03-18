7a5487ab69cc799f0f58fc15b44b0b4d4b3d2d80
Blake Dworaczyk <bdd4329@tamu.edu>
2025-03-18 16:21:51 -0500

    Better OpenWebUI API key creation logging

--- utils/create_openwebui_admin_api_key.sh
+++ utils/create_openwebui_admin_api_key.sh
@@ -5,22 +5,40 @@ FAILED=0
 rm -f $LOG_FILE
 
 # Authenticate to Cloudflare and get an access token
+echo "Authenticating to Cloudflare and getting an access token" >> $LOG_FILE
 CF_ACCESS_TOKEN=$(curl -X GET "https://${OPENWEBUI_FQDN}" \
   -H "Accept: application/json" \
   -H "CF-Access-Client-Id: ${CF_ACCESS_CLIENT_ID}" \
   -H "CF-Access-Client-Secret: ${CF_ACCESS_CLIENT_SECRET}" \
  -sS --dump-header - -o /dev/null | grep "CF_Authorization=" | \
  sed 's/set-cookie: //g' | cut -d';' -f1)
+ if [ $? -ne 0 ]
+then
+  echo "Failed to authenticate to Cloudflare and get an access token" >> $LOG_FILE
+  FAILED=1
+else
+  echo "Successfully authenticated to cloudflare and got access token" >> $LOG_FILE
+fi
  
  # Sign up for an account
  echo "Signing up for an OpenWebUI account" >> $LOG_FILE
- curl \
+ SIGNUP_RES=$(curl \
   -X POST "https://${OPENWEBUI_FQDN}/api/v1/auths/signup" \
   -H "accept: application/json" \
   -H "Content-Type: application/json" \
   -H "cookie: ${CF_ACCESS_TOKEN}" \
   --silent \
-  -d "{ \"email\": \"${OPENWEBUI_EMAIL}\", \"password\": \"${OPENWEBUI_PASSWORD}\", \"name\": \"Admin\" }" 2>&1 >> $LOG_FILE
+  -w "%{http_code}" \
+  -d "{ \"email\": \"${OPENWEBUI_EMAIL}\", \"password\": \"${OPENWEBUI_PASSWORD}\", \"name\": \"Admin\" }" 2>&1 | tee -a $LOG_FILE)
+  if [[ $? -eq 0 ]] && echo $SIGNUP_RES | egrep '200$|400$' > /dev/null
+  then
+    echo "" >> $LOG_FILE
+    echo "Successfully signed up for an OpenWebUI account" >> $LOG_FILE
+  else
+    echo "" >> $LOG_FILE
+    echo "Failed to sign up for an OpenWebUI account" >> $LOG_FILE
+    FAILED=1
+  fi
 
 # Start a cloudflared session to access the database
 echo "Starting cloudflared session to access the database" >> $LOG_FILE
